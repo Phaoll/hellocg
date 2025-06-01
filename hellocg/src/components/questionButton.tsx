d758a2f8a9d064eb6d8296bb24dcf4e12b831f8c
Phaoll <paul.corradini@orange.fr>
2025-06-01 20:04:50 +0200

    FIX: MVP complete

--- hellocg/src/components/questionButton.tsx
+++ hellocg/src/components/questionButton.tsx
@@ -1,5 +1,19 @@
-import type { FC } from "react";
+import { useState, type FC } from "react";
 import { Button } from "./ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "./ui/drawer";
+import { Minus, Plus } from "lucide-react";
+
+// TODO
+// - Change opacity of background when Drawer is drawn
 
 interface QuestionButtonProps {
   questionNumber: string;
@@ -12,29 +26,47 @@ const QuestionButton: FC<QuestionButtonProps> = ({
   question,
   answer,
 }) => {
-  const handleClick = () => {
-    alert(`${question} -> ${answer}`);
-  };
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   return (
-    <Button
-      onClick={handleClick}
-      className="w-full h-full min-h-32 min-w-32 aspect-square relative flex justify-center bg-gray-500"
-      type="button"
-      aria-label={`Question ${questionNumber}`}
-    >
-      <img
-        src="./helloPointLogo.png"
-        alt=""
-        className="w-full h-full min-w-8 min-h-8 object-cover rounded"
-      />
+    <Drawer open={isDrawerOpen}>
+      <DrawerTrigger asChild>
+        <Button
+          onClick={() => setIsDrawerOpen(true)}
+          className="w-full h-full min-h-32 min-w-32 aspect-square relative flex justify-center bg-gray-500"
+          type="button"
+          aria-label={`Question ${questionNumber}`}
+        >
+          <img
+            src="./helloPointLogo.png"
+            alt=""
+            className="w-full h-full min-w-8 min-h-8 object-cover rounded"
+          />
 
-      <div className="absolute inset-0 flex items-center justify-center z-10">
-        <span className="text-black font-bold text-4xl drop-shadow-md">
-          {questionNumber.replace("question", "")}
-        </span>
-      </div>
-    </Button>
+          <div className="absolute inset-0 flex items-center justify-center z-10">
+            <span className="text-black font-bold text-4xl drop-shadow-md">
+              {questionNumber.replace("question", "")}
+            </span>
+          </div>
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="flex flex-col mx-auto w-full items-center justify-center">
+          <DrawerHeader>
+            <DrawerTitle>{question}</DrawerTitle>
+          </DrawerHeader>
+          <details>
+            <summary>Réponse</summary>
+            <p>{answer}</p>
+          </details>
+          <DrawerFooter>
+            <Button onClick={() => setIsDrawerOpen(false)}>
+              La question, elle est répondue
+            </Button>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
   );
 };
 
